f9546621b6bc20de7e919787c286fe5046ed0e14
liandi <simdigliandi@gmail.com>
2020-03-08 17:59:36 +0700

    update chat to 2.0

--- src/Chat.js
+++ src/Chat.js
@@ -6,7 +6,6 @@ class Chat extends React.Component{
         super(props);
 
         this.state = {
-            username: '',
             message: '',
             messages: []
         };
@@ -22,9 +21,7 @@ class Chat extends React.Component{
         };
 
         this.sendMessage = ev => {
-            ev.preventDefault();
             this.socket.emit('SEND_MESSAGE', {
-                author: this.state.username,
                 message: this.state.message
             })
             this.setState({message: ''});
@@ -33,32 +30,23 @@ class Chat extends React.Component{
     }
     render(){
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-4">
-                        <div className="card">
-                            <div className="card-body">
-                                <div className="card-title">Global Chat</div>
-                                <hr/>
-                                <div className="messages">
-                                    {this.state.messages.map(message => {
-                                        return (
-                                            <div>{message.author}: {message.message}</div>
-                                        )
-                                    })}
-                                </div>
-
-                            </div>
-                            <div className="card-footer">
-                                <input type="text" placeholder="Username" value={this.state.username} onChange={ev => this.setState({username: ev.target.value})} className="form-control"/>
-                                <br/>
-                                <input type="text" placeholder="Message" className="form-control" value={this.state.message} onChange={ev => this.setState({message: ev.target.value})}/>
-                                <br/>
-                                <button onClick={this.sendMessage} className="btn btn-primary form-control">Send</button>
-                            </div>
-                        </div>
+            <div>
+                <section className="section">
+                    <div className="container">
+					{this.state.messages.map(message => {
+						return (
+						<div class="card">
+							<div class="card-content">
+								<div class="title">
+									{message.message}
+								</div>
+							</div>
+						</div>
+						)
+					})}
                     </div>
-                </div>
+                </section>
+                <input className="chat_input input input is-medium is-primary" onChange={event => this.setState({message: event.target.value})} onKeyPress={event => { if (event.key === 'Enter') { this.sendMessage() } }} type="text" placeholder="Masukkan Pesan" value={this.state.message} />
             </div>
         );
     }
